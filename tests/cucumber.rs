dabddff06e12508ece40fc6436b9c60b97253ee4
Prince Muel <68931805+princemuel@users.noreply.github.com>
2025-08-20 18:11:48 +0100

    test(feature): scaffold all project features

--- tests/cucumber.rs
+++ tests/cucumber.rs
@@ -1,15 +1,13 @@
-use cucumber::World;
+mod world;
+
+use cucumber::World as _;
+
+use crate::world::TestWorld;
 
 #[tokio::main]
 async fn main() {
-    RayTracerWorld::cucumber()
+    TestWorld::cucumber()
         .fail_on_skipped()
         .run_and_exit("tests/features/")
         .await;
-}
-
-#[derive(Debug, World)]
-pub struct RayTracerWorld {}
-impl Default for RayTracerWorld {
-    fn default() -> Self { todo!() }
 }
---
+++ tests/world.rs
@@ -0,0 +1,5 @@
+#[derive(Debug, cucumber::World)]
+pub struct TestWorld {}
+impl Default for TestWorld {
+    fn default() -> Self { todo!() }
+}
