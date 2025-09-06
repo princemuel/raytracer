8ef2dfcf99ac4f08e52f484a829a4b7e09cdc0eb
Prince Muel <68931805+princemuel@users.noreply.github.com>
2025-09-06 11:10:18 +0100

    test: add tests implementations for tuple arithmetic

--- tests/steps/tuple.rs
+++ tests/steps/tuple.rs
@@ -4,15 +4,17 @@ use raytracer::primitives::tuple::{Tuple, point, vector};
 use crate::support::world::TestWorld;
 
 // ------------------------------------------------------------------------------
-#[given(regex = r"^([a-zA-Z_][a-zA-Z0-9_]*) ← tuple\(([^,]+), ([^,]+), ([^,]+), ([^)]+)\)$")]
+#[given(
+    regex = r"^([a-zA-Z_][a-zA-Z0-9_]*) ← tuple\(([-+]?\d*\.?\d+), ([-+]?\d*\.?\d+), ([-+]?\d*\.?\d+), ([^)]+)\)$"
+)]
 async fn given_a_tuple(world: &mut TestWorld, name: String, x: f64, y: f64, z: f64, w: f64) {
     world.insert(&name, Tuple::new(x, y, z, w));
 }
-#[given(regex = r"^([a-zA-Z_][a-zA-Z0-9_]*) ← point\(([^,]+), ([^,]+), ([^)]+)\)$")]
+#[given(regex = r"^([a-zA-Z_][a-zA-Z0-9_]*) ← point\(([-+]?\d*\.?\d+), ([-+]?\d*\.?\d+), ([^)]+)\)$")]
 fn given_a_point(world: &mut TestWorld, name: String, x: f64, y: f64, z: f64) {
     world.insert(&name, point(x, y, z));
 }
-#[given(regex = r"^([a-zA-Z_][a-zA-Z0-9_]*) ← vector\(([^,]+), ([^,]+), ([^)]+)\)$")]
+#[given(regex = r"^([a-zA-Z_][a-zA-Z0-9_]*) ← vector\(([-+]?\d*\.?\d+), ([-+]?\d*\.?\d+), ([^)]+)\)$")]
 fn given_a_vector(world: &mut TestWorld, name: String, x: f64, y: f64, z: f64) {
     world.insert(&name, vector(x, y, z));
 }
@@ -62,7 +64,9 @@ fn then_is_not_vector(world: &mut TestWorld, name: String) {
 }
 
 // ------------------------------------------------------------------------------
-#[then(regex = r"^([a-zA-Z_][a-zA-Z0-9_]*) = tuple\(([^,]+), ([^,]+), ([^,]+), ([^)]+)\)$")]
+#[then(
+    regex = r"^([a-zA-Z_][a-zA-Z0-9_]*) = tuple\(([-+]?\d*\.?\d+), ([-+]?\d*\.?\d+), ([-+]?\d*\.?\d+), ([^)]+)\)$"
+)]
 fn then_variable_equals_tuple(world: &mut TestWorld, name: String, x: f64, y: f64, z: f64, w: f64) {
     let t = world.get::<Tuple>(&name).unwrap();
     let expected = Tuple::new(x, y, z, w);
@@ -70,13 +74,91 @@ fn then_variable_equals_tuple(world: &mut TestWorld, name: String, x: f64, y: f6
 }
 
 #[then(
-    regex = r"^([a-zA-Z_][a-zA-Z0-9_]*) \+ ([a-zA-Z_][a-zA-Z0-9_]*) = tuple\(([^,]+), ([^,]+), ([^,]+), ([^)]+)\)$"
+    regex = r"^([a-zA-Z_][a-zA-Z0-9_]*) \+ ([a-zA-Z_][a-zA-Z0-9_]*) = tuple\(([-+]?\d*\.?\d+), ([-+]?\d*\.?\d+), ([-+]?\d*\.?\d+), ([^)]+)\)$"
 )]
 fn then_add_equals_tuple(world: &mut TestWorld, a: String, b: String, x: f64, y: f64, z: f64, w: f64) {
     let t1 = world.get::<Tuple>(&a).unwrap();
     let t2 = world.get::<Tuple>(&b).unwrap();
 
     let actual = *t1 + *t2;
+    let expected = Tuple::new(x, y, z, w);
+
+    assert!(actual == expected, "Expected {:?}, got {:?}", expected, actual);
+}
+
+#[then(
+    regex = r"^([a-zA-Z_][a-zA-Z0-9_]*) \- ([a-zA-Z_][a-zA-Z0-9_]*) = vector\(([-+]?\d*\.?\d+), ([-+]?\d*\.?\d+), ([^)]+)\)$"
+)]
+fn then_sub_equals_vector(world: &mut TestWorld, a: String, b: String, x: f64, y: f64, z: f64) {
+    let t1 = world.get::<Tuple>(&a).unwrap();
+    let t2 = world.get::<Tuple>(&b).unwrap();
+
+    let actual = *t1 - *t2;
+    let expected = vector(x, y, z);
+
+    assert!(actual == expected, "Expected {:?}, got {:?}", expected, actual);
+}
+
+#[then(
+    regex = r"^([a-zA-Z_][a-zA-Z0-9_]*) \- ([a-zA-Z_][a-zA-Z0-9_]*) = point\(([-+]?\d*\.?\d+), ([-+]?\d*\.?\d+), ([^)]+)\)$"
+)]
+fn then_sub_equals_point(world: &mut TestWorld, a: String, b: String, x: f64, y: f64, z: f64) {
+    let t1 = world.get::<Tuple>(&a).unwrap();
+    let t2 = world.get::<Tuple>(&b).unwrap();
+
+    let actual = *t1 - *t2;
+    let expected = point(x, y, z);
+
+    assert!(actual == expected, "Expected {:?}, got {:?}", expected, actual);
+}
+
+#[then(
+    regex = r"^-([a-zA-Z_][a-zA-Z0-9_]*) = tuple\(([-+]?\d*\.?\d+), ([-+]?\d*\.?\d+), ([-+]?\d*\.?\d+), ([^)]+)\)$"
+)]
+fn then_negate_var_equals_tuple(world: &mut TestWorld, a: String, x: f64, y: f64, z: f64, w: f64) {
+    let t = *(world.get::<Tuple>(&a).unwrap());
+
+    let actual = -t;
+    let expected = Tuple::new(x, y, z, w);
+
+    assert!(actual == expected, "Expected {:?}, got {:?}", expected, actual);
+}
+
+#[then(
+    regex = r"^([a-zA-Z_][a-zA-Z0-9_]*) \* ([-+]?\d*\.?\d+) = tuple\(([-+]?\d*\.?\d+), ([-+]?\d*\.?\d+), ([-+]?\d*\.?\d+), ([^)]+)\)$"
+)]
+fn then_var_times_value_equals_tuple(
+    world: &mut TestWorld,
+    a: String,
+    v: f64,
+    x: f64,
+    y: f64,
+    z: f64,
+    w: f64,
+) {
+    let t = *(world.get::<Tuple>(&a).unwrap());
+
+    let actual = t * v;
+    let expected = Tuple::new(x, y, z, w);
+
+    assert!(actual == expected, "Expected {:?}, got {:?}", expected, actual);
+}
+
+#[then(
+    regex = r"^([a-zA-Z_][a-zA-Z0-9_]*) \/ ([-+]?\d*\.?\d+) = tuple\(([-+]?\d*\.?\d+), ([-+]?\d*\.?\d+), ([-+]?\d*\.?\d+), ([^)]+)\)$"
+)]
+fn then_var_divide_value_equals_tuple(
+    world: &mut TestWorld,
+    a: String,
+    v: f64,
+    x: f64,
+    y: f64,
+    z: f64,
+    w: f64,
+) {
+    let t = *(world.get::<Tuple>(&a).unwrap());
+
+    let actual = t / v;
     let expected = Tuple::new(x, y, z, w);
 
     assert!(actual == expected, "Expected {:?}, got {:?}", expected, actual);
